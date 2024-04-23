08bd927c63be15bdffb58c796982ea984fc74e86
Praduman Prajapati <praduman.cnd@gmail.com>
2024-04-24 01:45:05 +0530

    added script

---
+++ hadoop-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+############
+#Date: 24 Apr 2024
+#Author: Praduman
+############
+
+
+set +x
+
+git clone https://github.com/big-data-europe/docker-hadoop.git
+
+ls
+
+cd docker-hadoop/
+
+docker-compose up -d
+
+docker ps
+
+docker exec -it namenode /bin/bash
+
+hdfs dfs -mkdir -p /user/root/
+
+hdfs dfs -ls /user/root
+
+exit
+
+ls
+
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-mapreduce-examples/2.7.1/hadoop-mapreduce-examples-2.7.1-sources.jar
+
+
